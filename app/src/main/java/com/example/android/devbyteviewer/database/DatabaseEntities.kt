bb908b1a16ebab876a4cd71930fc76c23706793c
Steve Penza <slpenza@gmail.com>
2023-07-28 01:17:38 -0400

    Through lesson 11

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,29 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.lifecycle.Transformations.map
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+fun List<Video>.asDomainModel(): List<Video> {
+    return map {
+        Video(
+            url = it.url,
+            title = it.title,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
+    }
+}
